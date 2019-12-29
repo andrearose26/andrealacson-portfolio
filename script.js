60e677939acb08e3c8a9e042bfe44da2532f948d
andrearose26 <andreaa.lacson@gmail.com>
2019-12-29 17:13:04 -0500

    hover states added

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var typed = new Typed('#typed', {
+const typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     startDelay: 700,
     loop: true,
     typeSpeed: 39,
     backSpeed: 20,
     backDelay: 3400,
+});
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 700, 'linear');
 });
